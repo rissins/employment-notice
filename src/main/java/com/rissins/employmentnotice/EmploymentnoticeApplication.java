9203d4fd34022274bf004171cebc98ce277a4183
rissins <tnalsqqq@naver.com>
2022-12-06 11:48:05 +0900

    initial commit

---
+++ src/main/java/com/rissins/employmentnotice/EmploymentnoticeApplication.java
@@ -0,0 +1,13 @@
+package com.rissins.employmentnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentnoticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentnoticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rissins/employmentnotice/EmploymentnoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rissins.employmentnotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentnoticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
